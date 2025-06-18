eeaa73edd06b3f5ded4d26789deb070e2fe8c1f3
Aurora Dawn <aurora.dawn@trans.fish>
2025-06-18 19:18:52 -0400

    Update to 1.21.6

--- src/main/java/dev/enjarai/restartdetector/RestartDetector.java
+++ src/main/java/dev/enjarai/restartdetector/RestartDetector.java
@@ -13,7 +13,7 @@ import org.slf4j.LoggerFactory;
 
 public class RestartDetector implements ModInitializer {
 	public static final String MOD_ID = "restart_detector";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	private static int ticksToStop = -1;
 
